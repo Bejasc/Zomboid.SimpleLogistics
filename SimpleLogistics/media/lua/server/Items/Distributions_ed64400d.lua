79109f860e11e8b7906ba2232c4f8377f5481ebb
Ben Schulz <benjamin.schulz94@gmail.com>
2019-07-12 22:11:19 +0930

    Add existing files

---
+++ SimpleLogistics/media/lua/server/Items/Distributions_ed64400d.lua
@@ -0,0 +1,380 @@
+Distributions = Distributions or {};
+
+local distributionTable = 
+{
+	loggingfactory = 
+	{
+		crate = 
+		{
+			items = 
+			{
+				"Logistics.Pallet",0.7,
+				"Logistics.LumberStack",0.6,
+				"Logistics.LumberPallet",0.3,
+				"Logistics.LogPallet",0.3,
+			},
+		},
+	},
+	all = 
+	{
+		clothingrack = 
+		{
+			items = 
+			{
+			},
+		},
+		freezer = 
+		{
+			items = 
+			{
+			},
+		},
+		postbox = 
+		{
+			items = 
+			{
+			},
+		},
+		corn = 
+		{
+			items = 
+			{
+			},
+		},
+		logs = 
+		{
+			items = 
+			{
+			},
+		},
+		locker = 
+		{
+			items = 
+			{
+			},
+		},
+		fruitbusha = 
+		{
+			items = 
+			{
+			},
+		},
+		fruitbushb = 
+		{
+			items = 
+			{
+			},
+		},
+		fruitbushc = 
+		{
+			items = 
+			{
+			},
+		},
+		fruitbushd = 
+		{
+			items = 
+			{
+			},
+		},
+		fruitbushe = 
+		{
+			items = 
+			{
+			},
+		},
+		inventorymale = 
+		{
+			items = 
+			{
+			},
+		},
+		inventoryfemale = 
+		{
+			items = 
+			{
+			},
+		},
+		shelvesmag = 
+		{
+			items = 
+			{
+			},
+		},
+		desk = 
+		{
+			items = 
+			{
+			},
+		},
+		filingcabinet = 
+		{
+			items = 
+			{
+			},
+		},
+		stove = 
+		{
+			items = 
+			{
+			},
+		},
+		microwave = 
+		{
+			items = 
+			{
+			},
+		},
+		medicine = 
+		{
+			items = 
+			{
+			},
+		},
+		wardrobe = 
+		{
+			items = 
+			{
+			},
+		},
+		crate = 
+		{
+			items = 
+			{
+				"Logistics.Pallet",0.4,
+				"Logistics.LumberStack",0.3,
+				"Logistics.LumberPallet",0.2,
+				"Logistics.LogPallet",0.2,
+				"Logistics.MetalPallet",0.2,
+			},
+		},
+		counter = 
+		{
+			items = 
+			{
+			},
+		},
+		sidetable = 
+		{
+			items = 
+			{
+			},
+		},
+		fridge = 
+		{
+			items = 
+			{
+			},
+		},
+		vendingsnack = 
+		{
+			items = 
+			{
+			},
+		},
+		vendingpop = 
+		{
+			items = 
+			{
+			},
+		},
+		bin = 
+		{
+			items = 
+			{
+			},
+		},
+		officedrawers = 
+		{
+			items = 
+			{
+			},
+		},
+		metal_shelves = 
+		{
+			items = 
+			{
+				"Logistics.Pallet",0.6,
+				"Logistics.LumberStack",0.7,
+				"Logistics.LumberPallet",0.4,
+				"Logistics.LogPallet",0.3,
+				"Logistics.MetalPallet",0.4,
+			},
+		},
+		shelves = 
+		{
+			items = 
+			{
+			},
+		},
+		other = 
+		{
+			items = 
+			{
+			},
+		},
+	},
+	shed = 
+	{
+		counter = 
+		{
+			items = 
+			{
+				"Logistics.Pallet",0.8,
+				"Logistics.LumberStack",0.65,
+				"Logistics.LumberPallet",0.55,
+				"Logistics.LogPallet",0.5,
+				"Logistics.MetalPallet",0.35,
+			},
+		},
+		logs = 
+		{
+			items = 
+			{
+				"Logistics.LogPallet",0.7,
+				"Logistics.Pallet",0.4,
+				"Logistics.LumberStack",0.4,
+			},
+		},
+		other = 
+		{
+			items = 
+			{
+				"Logistics.Pallet",0.8,
+				"Logistics.LumberStack",0.65,
+				"Logistics.LumberPallet",0.55,
+				"Logistics.LogPallet",0.5,
+				"Logistics.MetalPallet",0.35,
+			},
+		},
+	},
+	garagestorage = 
+	{
+		all = 
+		{
+			items = 
+			{
+				"Logistics.Pallet",0.8,
+				"Logistics.LumberStack",0.5,
+				"Logistics.LumberPallet",0.5,
+				"Logistics.LogPallet",0.5,
+				"Logistics.MetalPallet",0.5,
+			},
+		},
+	},
+	toolstore = 
+	{
+		shelves = 
+		{
+			items = 
+			{
+				"Logistics.Pallet",0.8,
+				"Logistics.LumberStack",0.7,
+				"Logistics.LumberPallet",0.5,
+				"Logistics.LogPallet",0.5,
+				"Logistics.MetalPallet",0.6,
+			},
+		},
+		counter = 
+		{
+			items = 
+			{
+				"Logistics.Pallet",0.3,
+				"Logistics.LumberStack",0.5,
+				"Logistics.LumberPallet",0.3,
+				"Logistics.LogPallet",0.3,
+				"Logistics.MetalPallet",0.3,
+			},
+		},
+	},
+	storageunit = 
+	{
+		all = 
+		{
+			items = 
+			{
+				"Logistics.Pallet",0.8,
+				"Logistics.LumberStack",0.5,
+				"Logistics.LumberPallet",0.5,
+				"Logistics.LogPallet",0.5,
+				"Logistics.MetalPallet",0.5,
+			},
+		},
+	},
+	gardenstore = 
+	{
+		shelves = 
+		{
+			items = 
+			{
+			},
+		},
+		crate = 
+		{
+			items = 
+			{
+				"Logistics.Pallet",1,
+				"Logistics.LumberStack",0.7,
+				"Logistics.LumberPallet",0.7,
+				"Logistics.LogPallet",0.7,
+				"Logistics.MetalPallet",0.5,
+			},
+		},
+	},
+	farmstorage = 
+	{
+		all = 
+		{
+			items = 
+			{
+				"Logistics.Pallet",1,
+				"Logistics.LumberStack",0.7,
+				"Logistics.LumberPallet",0.7,
+				"Logistics.LogPallet",0.7,
+				"Logistics.MetalPallet",0.5,
+			},
+		},
+	},
+	Carpenter = 
+	{
+		counter = 
+		{
+			items = 
+			{
+				"Logistics.Pallet",0.6,
+				"Logistics.LumberStack",1,
+				"Logistics.LumberPallet",0.7,
+				"Logistics.LogPallet",0.4,
+				"Logistics.MetalPallet",0.2,
+			},
+		},
+	},
+	ToolsCache1 = 
+	{
+		ToolsBox = 
+		{
+			items = 
+			{
+			},
+		},
+		counter = 
+		{
+			items = 
+			{
+				"Logistics.Pallet",0.6,
+				"Logistics.LumberStack",0.4,
+				"Logistics.LumberPallet",0.3,
+				"Logistics.LogPallet",0.2,
+				"Logistics.MetalPallet",0.2,
+			},
+		},
+		Duffelbag = 
+		{
+			items = 
+			{
+			},
+		},
+	},
+}
+
+table.insert(Distributions, distributionTable);
---
+++ SimpleLogistics/media/lua/server/LogisticsExtra.lua
@@ -0,0 +1,17 @@
+--Return additional Nails from recycling a pallet
+function Logistics_RecyclePallet(items, result, player)
+    player:getInventory():AddItem("Base.Nails");      
+	player:getInventory():AddItem("Base.Nails");      
+	player:getInventory():AddItem("Base.Nails");      
+	player:getInventory():AddItem("Base.Nails");      
+	player:getInventory():AddItem("Base.Nails");      					
+end
+function Logistics_ReturnStack(items, result, player)
+    player:getInventory():AddItem("Base.Rope");      
+	player:getInventory():AddItem("Base.Rope");       					
+end
+function Logistics_ReturnPallet(items, result, player)
+    player:getInventory():AddItem("Base.Rope");      
+	player:getInventory():AddItem("Base.Rope");       		
+	player:getInventory():AddItem("Logistics.Pallet");       					
+end
